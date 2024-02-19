ee46bfb2ccac59acfc8381d8081e7279f25b3f5b
SouchenOu <soukainaouchenuai@gmail.com>
2024-02-19 03:17:06 -0500

    working on login page

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
 import React from "react";
 
 function login() {
-  return <div>login</div>;
+  return <div className=" flex justify-center items-center h-screen w-screen flex-col gap-6 bg-panel-header-background" style={{ backgroundColor: '#801336' }}>
+    <div className="flex justify-center items-center text-white gap-2">
+      <Image src="/chat.png" alt="chat" height={300} width={300}/>
+      <span className="text-7xl">soukeyChat</span>
+    </div>
+    
+    </div>;
 }
 
 export default login;
