95f9c4e11034f8231079ec458203ecb7a77c947a
SouchenOu <soukainaouchenuai@gmail.com>
2024-02-22 10:31:35 -0500

    working on menu context for changing avatar in register

--- client/src/components/usedFiles/Avatar.jsx
+++ client/src/components/usedFiles/Avatar.jsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import { FaCamera } from 'react-icons/fa';
+import ContextMenu from "./ContextMenu";
 
 function Avatar({type, image, setImage}) {
   const [hover, setHover] = useState(false);
+  const [isContextVisible, setIsContextVisible] = useState(false);
+  const [ContextMenuCoordinate, SetContextMenuCoordinate] = useState({x:0, y:0});
+
+  const showContextMenu = (e) =>{
+    /******When an event occurs, browsers often have default actions associated with that event. For example, clicking a link might navigate to a new page, submitting a form might cause a page reload, and pressing the "Enter" key in a text input might submit the form. event.preventDefault() allows you to stop these default actions from happening. */
+    e.preventDefault();
+    // SetContextMenuCoordinate({x : e.pageX, y : e.pageY});
+    setIsContextVisible(true);
+
+  }
+  const contextMenuOptions = [{name : "Take a picture", callback : () =>{}}]
   return (
     <>
       <div className="flex items-center justify-center">
@@ -20,13 +32,16 @@ function Avatar({type, image, setImage}) {
        )}
        {type === "xl" && (
         <div className="relative cursor-pointer z-0" onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)}>
-          <div className={`z-10 h-40 w-40  absolute  flex items-center justify-center gap-3 flex-col ${hover ? "visible" : "hidden" }` }>
-            <FaCamera className="text-2xl" id="text-opener"/>
+          <div className={`z-10 h-40 w-40  absolute  flex items-center justify-center gap-3 flex-col ${hover ? "visible" : "hidden" }` } onClick={(e)=>showContextMenu(e)}>
+            <FaCamera className="text-2xl" id="text-opener" onClick={(e)=>showContextMenu(e)}/>
             <span className=""  style={{ color: '#EE4540' }}>Change your picture</span>
           </div>
             <div className="h-40 w-40" >
-              <Image src={image} alt="avatar" className="rounded-full" fill/>
+              <Image src={image} alt="avatar" className="rounded-full" fill onClick={(e)=>showContextMenu(e)}/>
             </div>
+            {
+              isContextVisible && <ContextMenu options={contextMenuOptions} cordinates={ContextMenuCoordinate} contextMenu={isContextVisible} setContextMenu={setIsContextVisible}/>
+            }
 
         </div>
        )
--- client/src/components/usedFiles/ContextMenu.jsx
+++ client/src/components/usedFiles/ContextMenu.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 
-function ContextMenu() {
-  return <div>ContextMenu</div>;
+function ContextMenu({options, cordinates, contextMenu, setContextMenu}) {
+  const contextMenuRef = useRef(null);
+  const handleClick = (e, callback) => {
+
+  }
+  return <div className={`bg-dropdown-background fixed py-2 z-[100] top-[${cordinates.x}] left-[${cordinates.y}] shadow-xl` }  ref={contextMenuRef}>
+      <ul>
+       
+        {options.map(({name , callback})=>(
+            <li key={name} onClick={(e) => handleClick(e, callback)} className="px-5 py-2 cursor-pointer">
+              <span className="text-white">{name}</span>
+            </li> 
+
+        ))}
+      </ul>
+    </div>;
 }
 
 export default ContextMenu;
